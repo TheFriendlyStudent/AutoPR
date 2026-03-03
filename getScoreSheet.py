dd1972339fbc7151a5e308533a6bcf09c1c46da4
Rishi Boggavarapu <83251988+TheFriendlyStudent@users.noreply.github.com>
2026-03-03 15:29:06 -0600

    setup actions

--- getScoreSheet.py
+++ getScoreSheet.py
@@ -14,8 +14,18 @@ scope = [
     "https://www.googleapis.com/auth/drive"
 ]
 
-creds_dict = json.loads(os.environ["GOOGLE_CREDENTIALS"])
-creds = Credentials.from_service_account_info(creds_dict, scopes=scope)
+
+if "GOOGLE_CREDENTIALS" in os.environ:
+    # Running in GitHub Actions
+    creds_dict = json.loads(os.environ["GOOGLE_CREDENTIALS"])
+    creds = Credentials.from_service_account_info(creds_dict, scopes=scope)
+else:
+    # Running locally
+    creds = Credentials.from_service_account_file(
+        "secrets//autopr-489119-5c3d04856538.json",
+        scopes=scope
+    )
+
 client = gspread.authorize(creds)
 
 spreadsheet = client.open_by_key("1UJzab8BwMgScaYoLqTBKk_8sDy7Y7cNp3YqrrZhy38I")
